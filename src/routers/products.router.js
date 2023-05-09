18e1ba01bd0ce09d4863f315d288f69da4ab724c
Mariana Budani <mariana.budani@gmail.com>
2023-05-09 18:05:16 -0300

    models

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -13,4 +13,18 @@ productsRouter.post('/', (req, res) => {
   res.status(201).send(product)
 })
 
+productsRouter.get('/:pid', (req, res) => {
+  const id = req.params.pid
+  const filterId = await productManager.getProductsById(id)
+  try{
+    if(filterId != undefined){
+      return res.send(filterId)
+    }else{
+      res.status(200).send(await filterId)
+    }
+  }catch(error){
+    res.status(400).send(`400 ${error}`)
+  }
+})
+
 export { productsRouter }
